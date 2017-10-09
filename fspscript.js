aa127d289b915a2d5c6b967f16166392774c848e
sharonzachariah <31422640+sharonzachariah@users.noreply.github.com>
2017-10-09 01:42:09 -0400

    Add files via upload

---
+++ fspscript.js
@@ -0,0 +1,150 @@
+
+function inspire()
+{
+	document.getElementById('p1').src = 'p1s.png';
+
+}
+function inspire1()
+{
+	document.getElementById('p2').src = 'p2s.png';
+
+}
+function inspire2()
+{
+	document.getElementById('p3').src = 'p3s.png';
+
+}
+function inspire3()
+{
+	document.getElementById('p4').src = 'p4s.png';
+
+}
+function inspire4()
+{
+	document.getElementById('p5').src = 'p5s.png';
+
+}
+function inspire5()
+{
+	document.getElementById('p6').src = 'p6s.png';
+
+}
+
+function remove()
+		{
+			document.getElementById('box').style.visibility = "hidden";
+            document.getElementById('x').style.visibility = "hidden";
+            document.getElementById('add').style.visibility = "hidden";
+            document.getElementById('title1').style.visibility = "hidden";
+            document.getElementById('cart').src = 'cart.png';
+			alert('Order removed from cart.');
+		}
+
+function ss() 
+		{
+			document.getElementById('sofa').src = 'sofa1.png';
+			document.getElementById('bed').src = 'bed.png';
+			document.getElementById('round').src = 'round.png';
+			document.getElementById('floor').src = 'floor.png';
+		}
+
+function bb() 
+		{
+			document.getElementById('sofa').src = 'sofa.png';
+			document.getElementById('bed').src = 'bed1.png';
+			document.getElementById('round').src = 'round.png';
+			document.getElementById('floor').src = 'floor.png';
+		}
+
+function rr() 
+		{
+			document.getElementById('sofa').src = 'sofa.png';
+			document.getElementById('bed').src = 'bed.png';
+			document.getElementById('round').src = 'round1.png';
+			document.getElementById('floor').src = 'floor.png';
+		}
+
+function ff() 
+		{
+			document.getElementById('sofa').src = 'sofa.png';
+			document.getElementById('bed').src = 'bed.png';
+			document.getElementById('round').src = 'round.png';
+			document.getElementById('floor').src = 'floor1.png';
+		}
+
+function sq() 
+		{
+			document.getElementById('sqr').src = 'sqr1.png';
+			document.getElementById('round').src = 'round.png';
+			document.getElementById('dog').src = 'dog.png';
+			document.getElementById('bear').src = 'bear.png';
+			document.getElementById('bunny').src = 'bunny.png';
+			document.getElementById('cat').src = 'cat.png';
+			document.getElementById('custom').src = 'custom.png';
+		}
+
+function ro() 
+		{
+			document.getElementById('sqr').src = 'sqr.png';
+			document.getElementById('round').src = 'round1.png';
+			document.getElementById('dog').src = 'dog.png';
+			document.getElementById('bear').src = 'bear.png';
+			document.getElementById('bunny').src = 'bunny.png';
+			document.getElementById('cat').src = 'cat.png';
+			document.getElementById('custom').src = 'custom.png';
+		}
+
+function dogg() 
+		{
+			document.getElementById('sqr').src = 'sqr.png';
+			document.getElementById('round').src = 'round.png';
+			document.getElementById('dog').src = 'dog1.png';
+			document.getElementById('bear').src = 'bear.png';
+			document.getElementById('bunny').src = 'bunny.png';
+			document.getElementById('cat').src = 'cat.png';
+			document.getElementById('custom').src = 'custom.png';
+		}
+
+function be() 
+		{
+			document.getElementById('sqr').src = 'sqr.png';
+			document.getElementById('round').src = 'round.png';
+			document.getElementById('dog').src = 'dog.png';
+			document.getElementById('bear').src = 'bear1.png';
+			document.getElementById('bunny').src = 'bunny.png';
+			document.getElementById('cat').src = 'cat.png';
+			document.getElementById('custom').src = 'custom.png';
+		}
+
+function bu() 
+		{
+			document.getElementById('sqr').src = 'sqr.png';
+			document.getElementById('round').src = 'round.png';
+			document.getElementById('dog').src = 'dog.png';
+			document.getElementById('bear').src = 'bear.png';
+			document.getElementById('bunny').src = 'bunny1.png';
+			document.getElementById('cat').src = 'cat.png';
+			document.getElementById('custom').src = 'custom.png';
+		}
+
+function catt() 
+		{
+			document.getElementById('sqr').src = 'sqr.png';
+			document.getElementById('round').src = 'round.png';
+			document.getElementById('dog').src = 'dog.png';
+			document.getElementById('bear').src = 'bear.png';
+			document.getElementById('bunny').src = 'bunny.png';
+			document.getElementById('cat').src = 'cat1.png';
+			document.getElementById('custom').src = 'custom.png';
+		}
+
+function cu() 
+		{
+			document.getElementById('sqr').src = 'sqr.png';
+			document.getElementById('round').src = 'round.png';
+			document.getElementById('dog').src = 'dog.png';
+			document.getElementById('bear').src = 'bear.png';
+			document.getElementById('bunny').src = 'bunny.png';
+			document.getElementById('cat').src = 'cat.png';
+			document.getElementById('custom').src = 'custom1.png';
+		}
